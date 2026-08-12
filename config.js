407c6a2c9004e4c6f24a9cf68cf3c53902318dd9
sbsopex <sbs.opex@gmail.com>
2026-08-12 09:40:57 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// TURN TIME DASHBOARD - CENTRAL CONNECTION CONFIG
+// Isi URL deployment Google Apps Script yang BERAKHIR /exec.
+// Setelah file ini diupload ke GitHub bersama index.html, semua perangkat akan auto-connect.
+window.TURNTIME_CONFIG = {
+  scriptUrl: 'https://script.google.com/macros/s/AKfycbw96ZMcxYh3bH3HF1DbXIUA_qqz8N3WJtrPqpCnQCDjudaAMT3U00WjYkARN2mY-oF9kg/exec'
+};
